8083185bf074e767ab0a7ca66b3cc38a4f839856
root <root@ip-172-31-89-120.ec2.internal>
2020-01-21 22:36:14 +0000

    Prueba

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,6 +17,6 @@ public class Greeter {
  *@return greetings string
  */
   public final String greet(final String someone) {
-    return String.format("Hello, %s!", someone);
+    return String.format("Hello....., %s!", someone);
   }
 }
